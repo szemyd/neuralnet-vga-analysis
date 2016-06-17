1f145b17ed83f7ff1595ad247d6c6376b790b3c6
D Sz <D Sz>
2016-06-17 17:42:05 +0100

    Getting more interesting patterns

--- First_Eclipse/src/firsteclipse/ManageBoxes.java
+++ First_Eclipse/src/firsteclipse/ManageBoxes.java
@@ -1,5 +1,7 @@
 package firsteclipse;
 
+import java.util.Random;
+
 import processing.core.PApplet;
 import processing.core.PVector;
 
@@ -44,9 +46,9 @@ public class ManageBoxes {
 			}
 		}
 
-		// for (int i = 0; i < 10; i++) {
-		checkNeighbourhood();
-		// }
+	for (int i = 0; i < 5; i++) {
+			checkNeighbourhood();
+	}
 	}
 
 	public void checkNeighbourhood() {
@@ -56,8 +58,8 @@ public class ManageBoxes {
 		for (int i = 0; i < boxes.length; i++) {
 			for (int j = 0; j < boxes[i].length; j++) {
 				boxes[i][j].height = walls(i, j);
-				p.println(boxes[i][j].height);
-				// boxes[i][j].height = separation(i, j);
+				// p.println(boxes[i][j].height);
+				boxes[i][j].height = separation(i, j);
 			}
 		}
 		// for (int i = hArray.length - 1; i > 0; i--) {
@@ -87,7 +89,7 @@ public class ManageBoxes {
 	}
 
 	public float choose(float chance) {
-		if (chance > 60.0f) {
+		if (chance > 98.0f) {
 			return 1.0f;
 		} else
 			return 0.0f;
@@ -105,13 +107,28 @@ public class ManageBoxes {
 			}
 		}
 
-		if (counter < 2)
+		if (counter < 3)
 			return 0f;
 		else
 			return 1.0f;
 	}
 
-	// public float separation(int i, int j) {
+	public float separation(int i, int j) {
+		for (int k = -2; k < 3; k++) {
+			for (int l = -2; l < 3; l++) {
+				if (k < -1 && k > 1 && l < -1 && l > 1) {
+					if ((i + k) >= 0 && (j + l) >= 0 && (i + k) < boxes.length && (j + l) < boxes[i].length) {
+						if (boxes[i + k][j + l].height > 0) {
+							if (p.random(100) > 50f) {
+								boxes[i + k][j + l].height = 0.0f;
+							} else
+								return 0.0f;
+						}
 
-	// }
+					}
+				}
+			}
+		}
+		return boxes[i][j].height;
+	}
 }
