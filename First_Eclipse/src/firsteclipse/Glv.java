41ff124c54df38ee9e0dd878ddfdf68c5b7a8897
D Sz <D Sz>
2016-06-21 17:23:01 +0100

    Corrected positioning

--- First_Eclipse/src/firsteclipse/Glv.java
+++ First_Eclipse/src/firsteclipse/Glv.java
@@ -3,7 +3,7 @@ package firsteclipse;
 import processing.core.PShape;
 
 public class Glv {
-	public static float cubeSize=3f;
+	public static float cubeSize=2f;
 	
 	public static float roomSizeX = 129.4f;
 	public static float roomSizeY = 160.42f;
